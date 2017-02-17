96f8b0f291bf6d066d78a9fa9de63cbfe47e6a8c
PabloMansilla <pmansilla32@gmail.com>
2017-02-17 12:36:47 -0300

    falta solo el juego

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,15 +1,38 @@
-function animacion() {
-  function efecto1Titulo() {
-    $("#titulo1").animate({color:"red"}, 1000, "linear", efecto2Titulo);
-  };
-  function efecto2Titulo() {
-    $("#titulo1").animate({color:"white"}, 1000, "linear", efecto1Titulo);
-  };
+function inicioTitulo() {
+  $("#titulo1").switchClass("main-titulo","estilo-titulo",1000,"easeInOutBounce");
+};
+function finTitulo() {
+  $("#titulo1").switchClass("estilo-titulo","main-titulo",1000,"easeInOutBounce");
+};
 
-}
+
+function finJuego() {
+
+
+  $("#tablaMarcadores").switchClass("panel-score","panel-2-score",1000,"easeInOutBounce");
+  $("#tablero").switchClass("panel-tablero","panel-2-tablero",1000,"easeInOutBounce");
+  $("#time2").switchClass("time","tiempo2",1000,"easeOutBounce");
+
+  //jQuery('h1').prependTo('#tablaMarcadores').attr("id","finish").addclass("termin");
+
+  $( "#tablaMarcadores").prepend('<h1 id="finish" class="termin">Juego Terminado</h1>');
+
+  //$("#finish").append("Juego Terminado");
+
+};
 
 $(document).ready(function () {
 
-  animacion();
+  var myVar = setInterval(function() {inicioTitulo(), finTitulo() }, 1000);
 
+
+  $("#boton1").click(function () {
+
+  $('.timer').startTimer({
+  onComplete: function () {
+    finJuego();
+  }
+});
+
+  });
 });
